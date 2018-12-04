368f61733529c10ff783f18fc18153f8365d05e8
Rohan Prakash <rohanp3425@gmail.com>
2018-12-04 12:11:25 -0600

    new shit

---
+++ app/src/main/java/com/example/jeeva/myapplication/ActivityPG2.java
@@ -0,0 +1,16 @@
+package com.example.jeeva.myapplication;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ActivityPG2 extends Activity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question1);
+    }
+
+}
--- app/src/main/java/com/example/jeeva/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jeeva/myapplication/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.jeeva.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = findViewById(R.id.getStartedButton);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, ActivityPG2.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
