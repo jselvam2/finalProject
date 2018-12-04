039e89b8a4e2c68bb2a287d86880fbe3373fcf5e
Rohan Prakash <rohanp3425@gmail.com>
2018-12-03 18:39:36 -0600

    did the first question page

--- app/src/main/java/com/example/jeeva/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jeeva/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jeeva.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button startApp = findViewById(R.id.getStartedButton);
     }
 }
