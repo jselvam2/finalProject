6686f133f0f91e741b5310a6a8f4331943c31921
Rohan Prakash <rohanp3425@gmail.com>
2018-12-06 15:17:06 -0600

    updates

--- app/src/main/java/com/example/jeeva/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jeeva/myapplication/MainActivity.java
@@ -10,34 +10,63 @@ import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    private ArrayList<String> responses = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addListenerOnButton();
+
+        Button start = findViewById(R.id.getStartedButton);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                question1();
+                startActivity(new Intent(getApplicationContext(),ActivityPG2.class));
+            }
+        });
+
+        Button A = findViewById(R.id.genreA);
+        Button B = findViewById(R.id.genreB);
+        Button C = findViewById(R.id.genreC);
+        Button D = findViewById(R.id.genreD);
+
+        A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PG3.class));
+            }
+        });
+        B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PG3.class));
+            }
+        });
+        C.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PG3.class));
+            }
+        });
+        D.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PG3.class));
+            }
+        });
     }
 
-    public void addListenerOnButton() {
+    public void question1() {
 
-        final Context context = this;
-
-        button = findViewById(R.id.getStartedButton);
-
-        button.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                Intent intent = new Intent(context, ActivityPG2.class);
-                startActivity(intent);
-
-            }
-
-        });
+        Intent intent = new Intent(this, ActivityPG2.class);
+        startActivity(intent);
 
     }
 }
--- app/src/main/java/com/example/jeeva/myapplication/PG3.java
+++ app/src/main/java/com/example/jeeva/myapplication/PG3.java
@@ -4,14 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
-public class ActivityPG2 extends AppCompatActivity {
-
+public class PG3 extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.question1);
+        setContentView(R.layout.question2);
 
 
     }
-
 }
